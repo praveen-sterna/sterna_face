62f256de0f7ab2460fec06d319e82717d140600d
Praveen B <sterna@Praveens-MacBook-Air.local>
2024-08-30 11:18:58 +0530

    fixed bugs

--- lib/face_registration_detector.dart
+++ lib/face_registration_detector.dart
@@ -93,7 +93,7 @@ class _FaceRegistrationDetectorViewState extends State<FaceRegistrationDetectorV
     }else if( (face.leftEyeOpenProbability ?? 0.0) < 0.5){
       _msg = "Open your right eye";
     }else if(_angle == FaceAngle.left) {
-      if(Platform.isIOS && (headAngle < -45 || headAngle >= 0)){
+      if(Platform.isIOS && headAngle > -45){
         _msg = _turnLeft;
       }else if(Platform.isAndroid){
 
